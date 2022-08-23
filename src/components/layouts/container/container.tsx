453833a21ae3aa3fdffd8db207595a5ace092df4
Kentaro Otsuka <otsuka@zizo.ne.jp>
2022-08-23 12:08:21 +0900

    add : TopButtonコンポーネントを追加

--- src/components/layouts/container/container.tsx
+++ src/components/layouts/container/container.tsx
@@ -1,14 +1,22 @@
 import { FC, ReactElement } from 'react';
 
+import { TopButton } from '../../elements';
+
 import styles from './styles.module.scss';
 
 type TProps = {
+  isShowTopButton?: boolean;
   children: ReactElement;
 };
 
 const Container: FC<TProps> = (props: TProps) => {
-  const { children }: TProps = props;
-  return <div className={styles.container}>{children}</div>;
+  const { children, isShowTopButton }: TProps = props;
+  return (
+    <div className={styles.container}>
+      {isShowTopButton && <TopButton />}
+      {children}
+    </div>
+  );
 };
 
 export { Container };
